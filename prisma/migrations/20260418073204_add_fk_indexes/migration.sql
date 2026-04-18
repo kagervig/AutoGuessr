91ecc281973516ed1e3b999f2d57cfafd0d8bcfa
Erin Gallagher <erin.catherine.gallagher@gmail.com>
2026-04-18 15:40:31 +0800

    add index

---
+++ prisma/migrations/20260418073204_add_fk_indexes/migration.sql
@@ -0,0 +1,8 @@
+-- CreateIndex
+CREATE INDEX "Image_vehicleId_idx" ON "Image"("vehicleId");
+
+-- CreateIndex
+CREATE INDEX "Round_gameId_idx" ON "Round"("gameId");
+
+-- CreateIndex
+CREATE INDEX "VehicleAlias_vehicleId_idx" ON "VehicleAlias"("vehicleId");
